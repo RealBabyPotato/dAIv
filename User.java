b7ae6d556bda6bc7e82f8e5ac07827b59a8adc6a
Various72 <120766872+Various72@users.noreply.github.com>
2024-03-13 09:15:55 -0700

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+public class User {
+    private int phoneNum;
+    
+    public User(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    private void writeToFile() {
+
+    }
+
+    private void readFromFile() {
+
+    }
+
+    
+}
