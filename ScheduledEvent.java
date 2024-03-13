17ed21aaa03b36dc7e68fa8189ee934158dcf33d
s-zheng-z <153850088+s-zheng-z@users.noreply.github.com>
2024-03-13 16:34:31 -0700

    Add files via upload

---
+++ ScheduledEvent.java
@@ -0,0 +1,37 @@
+package daiv;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class ScheduledEvent{
+	
+	private Timer timeTracker;
+	private TimerTask task;
+	private long timeToSend;
+	private String response;
+	private int identifier;
+	
+	public ScheduledEvent(int id, String r, long d) {
+		
+		identifier = id;
+		response = r;
+		timeToSend = d;
+		
+		task = new TimerTask() {
+			public void run() {
+				//do the do
+				System.out.println("aaaaaa");
+			}
+		};
+		
+		//get timeToSend by asking for milliseconds from chat
+		timeTracker = new Timer();
+		timeTracker.schedule(task, timeToSend);
+		
+	}
+	
+	
+	
+	
+}
