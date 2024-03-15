4c55c7cc679dd8bf92b2a47f24557fc9ef7ac0d4
Baby Potato <jlambn@shaw.ca>
2024-03-15 11:09:17 -0700

    update dependencies

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
-import org.eclipse.jetty.util.ajax.JSON;
 import org.json.simple.parser.ParseException;
 
 class Main{
@@ -10,15 +9,12 @@ class Main{
 
         // this is how we register a new user atm
         User jaden = new User(new PhoneNumber("2508809769"), "Jaden");
-        User ethan = new User(new PhoneNumber("7785334028"), "Ethan");
+        //User ethan = new User(new PhoneNumber("7785334028"), "Ethan");
 
         JSONManager jadenConversation = new JSONManager(jaden.userName);
         jadenConversation.addConversation("test test", "reply reply");
         jadenConversation.read_File(jaden.userName);
 
-        
-        //TwilioSendMessageExample.messageUser(ethan, "ethan is lame and cool and blah blah blah");
-        
         // this is how we send a message to a user. this method will likely be moved out of the TwilioSendMessageExample class soon.
         TwilioSendMessageExample.messageUser(jaden, "This is an example of a text to the User object Jaden.");
     }
