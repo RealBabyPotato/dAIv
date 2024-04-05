58eb33f50c3ec4ddea8a71a166528065da8f6341
s-zheng-z <153850088+s-zheng-z@users.noreply.github.com>
2024-04-04 19:35:10 -0700

    Update EventMaker.java
    methods now return false if date set is in the past

--- EventMaker.java
+++ EventMaker.java
@@ -1,15 +1,39 @@
-package daiv;
-
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.Calendar;
 
 public class EventMaker {
 
-	public static void makeEvent(String r, Calendar d) {
+	public static boolean makeEvent(String r, Calendar d) {
+		if(getTimeDiff(d) < 0) {
+			return false;
+		}
 		ScheduledEvent a = new ScheduledEvent(r, d);
+		return true;
 	}
 	
-	public static void makeEvent(String r, Calendar d, long rep) {
+	public static boolean makeEvent(String r, Calendar d, long rep) {
+		if(getTimeDiff(d) < 0) {
+			return false;
+		}
 		ScheduledEvent a = new ScheduledEvent(r, d, rep);
+		return true;
 	}
+	
+	public static long getTimeDiff(Calendar cal) {
+		Clock clocka = Clock.system(ZoneId.of("UTC"));
+		Clock clock = Clock.offset(clocka, Duration.ofHours(-7));
+		
+		Instant start = Instant.now(clock);
+		
+		String s = cal.toInstant().toString();
+		CharSequence cs = s;
+		Instant end = Instant.parse(cs);
+
+		return Duration.between(start, end).toMillis();
+		
+	}	
 	
 }
