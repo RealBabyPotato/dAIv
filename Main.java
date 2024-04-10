0f32dee1e108e1deee7ea62633b22058be2e6e69
RealBabyPotato <105143726+RealBabyPotato@users.noreply.github.com>
2024-04-10 16:08:54 -0700

    remove janky region

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import com.twilio.Twilio;
-import com.twilio.base.ResourceSet;
 import com.twilio.type.PhoneNumber;
 import com.twilio.rest.api.v2010.account.Message;
 
@@ -22,35 +21,4 @@ class Main{
         jaden.writeToFile("Test");
         jaden.readFromFile();
     }
-
-    //region super janky
-    // !!! this is a *temporary* workaround to not having access to webhooks and the server while will/mr. h are away! this is a very bad implementation of what it's trying to do. it is for debug purposes only.
-    // WILL DELETED IT BECAUSE IT WAS BROKE AH
-
-    // refreshes the most recent message and tells us if it has changed.
-    private boolean refreshMostRecentMessage(){
-        ResourceSet<Message> messages = Message.reader().limit(1).read();
-
-        for(Message record : messages) {
-            if(!record.equals(mostRecentMessage)){
-                this.mostRecentMessage = record;
-                System.out.println("Updating MRM");
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
-
-    private User findUser(PhoneNumber num){
-        for(User user : RegisteredUsers){
-            System.out.println(user.phoneNumber);
-            if(user.phoneNumber.equals(num)){
-                return user;
-            }
-        }
-
-        return null;
-    }
-    //endregion
 }
