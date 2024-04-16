c256b7d072b07da29c53743a6a70a21095c9ff93
RealBabyPotato <105143726+RealBabyPotato@users.noreply.github.com>
2024-04-16 11:00:23 -0700

    Fix PopulateUsers method

--- backup.java
+++ backup.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class backup {
-    private final Gson gson = new GsonBuilder().setPrettyPrinting()
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting()
             .excludeFieldsWithoutExposeAnnotation()
             .create();
 
@@ -40,12 +40,8 @@ public class backup {
         saveUsersToJSON(users);
     }
 
-
-
-
-
     // Method to return a list of User objects from JSON file
-    public ArrayList<User> getUsersFromJSON() {
+    public static ArrayList<User> getUsersFromJSON() {
         ArrayList<User> users = new ArrayList<>();
         try (Reader reader = new FileReader("users.json")) {
             Type userListType = new TypeToken<ArrayList<User>>() {}.getType();
