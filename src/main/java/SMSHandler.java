3cb58cc2c0adb56dc8be7f4270d433f37e173943
RealBabyPotato <jlambn@shaw.ca>
2024-06-29 19:16:23 -0700

    further cleanup

--- src/main/java/SMSHandler.java
+++ src/main/java/SMSHandler.java
@@ -6,84 +6,67 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
-import javax.naming.NameNotFoundException;
-// import com.sun.net.httpserver.HttpServer;
-// import com.twilio.rest.api.v2010.account.IncomingPhoneNumber;
-
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 
 import java.net.URLDecoder;
 
 public class SMSHandler implements HttpHandler {
-// This overrides the Java HTTPHandler to Listen for an HTTP Request and produce a different response
+    // This overrides the Java HTTPHandler to Listen for an HTTP Request and produce a different response
+    @Override
+    public void handle(HttpExchange t) throws IOException {
 
-@Override 
-public void handle(HttpExchange t) throws IOException { 
+      System.out.println("I got a message!");
 
-  System.out.println("I got a message!");
-  InputStream stream = t.getRequestBody();
-   String str_full = new BufferedReader(
-     new InputStreamReader(stream, "UTF-8"))
-     .lines()
-     .collect(Collectors.joining("\n"));
-    System.out.println(str_full);  
- // extract the Body of the HTTPRequest from Twilio to String
-   int str_start = str_full.indexOf("&Body")+6;
-   String str = str_full.substring(str_start);
-   int str_end = str.indexOf("&");
-   String incoming_message = URLDecoder.decode(str.substring(0,str_end), "UTF-8"); 
-   //System.out.println(incoming_message);
- // extract the incoming phone# of the HTTPRequest from Twilio to String
-   str_start = str_full.indexOf("&From=%2B")+10;
-   str = str_full.substring(str_start);
-   str_end = str.indexOf("&");
-   String incoming_phone = str.substring(0,str_end);
+      InputStream stream = t.getRequestBody();
+      String str_full = new BufferedReader(
+         new InputStreamReader(stream, "UTF-8"))
+         .lines()
+         .collect(Collectors.joining("\n"));
+
+      System.out.println(str_full);
+      // extract the Body of the HTTPRequest from Twilio to String
+
+      int str_start = str_full.indexOf("&Body")+6;
+      String str = str_full.substring(str_start);
+      int str_end = str.indexOf("&");
+      String incoming_message = URLDecoder.decode(str.substring(0,str_end), "UTF-8");
+
+      // extract the incoming phone# of the HTTPRequest from Twilio to String
+      str_start = str_full.indexOf("&From=%2B")+10;
+      str = str_full.substring(str_start);
+      str_end = str.indexOf("&");
+      String incoming_phone = str.substring(0,str_end);
 
 
-// create a Twilio xml reponse string that echos the incoming text
-System.out.println(incoming_phone);
+       // create a Twilio xml reponse string that echos the incoming text
+       System.out.println(incoming_phone);
 
-//testing
+       boolean userExists = false;
 
-/*User dave = new User(new PhoneNumber(incoming_phone), "Buddy");
-
-        // here is how we can use both twilio and chatgpt to send a message from chatgpt through twilio through our user! very cool!
-        //jaden.message(GPTAPI.sendAndReceive(jaden, "How much wood would a woodchuck chuck if a woodchuck could chuck wood?").replaceAll("\\\\n", "\n")); // the replaceAll here just makes the \n function properly
-dave.message(GPTAPI.sendAndReceive(dave, incoming_message));*/
-    boolean tempFlag = false;
-
-    for(User u : Main.RegisteredUsers){
-        if(u.getPhoneNumber().toString().equals(incoming_phone)){
-            u.message(GPTAPI.sendAndReceive(u, incoming_message));
-            tempFlag = true;
-            break;
+        for(User u : Main.RegisteredUsers){
+            if(u.getPhoneNumber().toString().equals(incoming_phone)){
+                u.message(GPTAPI.sendAndReceive(u, incoming_message));
+                userExists = true;
+                break;
+            }
         }
+
+        if(!userExists){
+          //TwilioSendMessageExample.messageUser(new User(new PhoneNumber(incoming_phone), "Unknown"), "Unknown user; you aren't registered!");
+          // TwilioSendMessageExample.messageUser(new User(new PhoneNumber(incoming_phone)), "nknown"), "Unknown user; you aren't in RegisteredUsers!");
+
+          User.registerUser(new PhoneNumber(incoming_phone), incoming_message);
+        }
+
+        String response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message>" + getReply(incoming_message) + "</Message></Response>";
+
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
-
-    if(!tempFlag){
-      //TwilioSendMessageExample.messageUser(new User(new PhoneNumber(incoming_phone), "Unknown"), "Unknown user; you aren't registered!");
-      // TwilioSendMessageExample.messageUser(new User(new PhoneNumber(incoming_phone)), "nknown"), "Unknown user; you aren't in RegisteredUsers!");
-      User.registerUser(new PhoneNumber(incoming_phone), incoming_message);
+    public String getReply(String msg) {
+          return "You just said:" + msg;
     }
-
-//System.out.print(TwilioServer.send("2506613358",incoming_message));
-
-String response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message>" + getReply(incoming_message) + "</Message></Response>";
-//System.out.println(response);
-
-
-
-t.sendResponseHeaders(200, response.length());
-OutputStream os = t.getResponseBody();
-os.write(response.getBytes());
-os.close();
-
-} 
-  
-public String getReply(String msg) {
-      return "You just said:" + msg;
-} 
   
  }
