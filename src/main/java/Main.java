620bd147eaf3d35d050727c3c02f3baa6a447927
Jaden <105143726+RealBabyPotato@users.noreply.github.com>
2024-07-04 04:38:39 +0000

    confirm suspicion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,30 +19,6 @@ public class Main {
     // this effectively loads everything from our backup into memory. (including events!!)
     User.PopulateUsers(); // adds all users into memory - our 'registeredUsers' static list
 
-    //User jaden = new User(new PhoneNumber("2508809769"), "Jaden");
-    // System.out.println(GPTAPI.sendAndReceive(jaden, "in 5 minutes remind me to wash my hands"));
-    //backup.updateAndSaveUser(j);
-
-    /*User new_user = User.registerUser(new PhoneNumber("2508809769")); // add user to main.user and backup
-    SetupManager.setup(new_user, 0, "test startup message");
-
-    System.out.println(new_user.getUserName());
-
-    new_user.setUsername("jad");
-
-    System.out.println(new_user.getUserName());
-
-    SetupManager.setup(new_user, new_user.getSetupPhase(), "message");*/
-    
-    // System.out.println(GPTAPI.sendAndReceive(j, "test"));
-
-    //System.out.println(TwilioServer.send("+12506613358","HTTP Service is Running!"));
-
-    /*Date asdf = new Date();
-    asdf.setTime(System.currentTimeMillis() + 10 * 1000);
-
-    Reminder reminder = new Reminder(j, asdf, "get some bread at fairway's");*/
-
     TwilioServer service = new TwilioServer();
   }
 }
